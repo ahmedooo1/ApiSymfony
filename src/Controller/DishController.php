e47b676bd13495d375cbc22b4597105c3b6f5953
ahmedooo1 <aahmadooo997@gmail.com>
2024-05-25 17:42:08 +0200

    Update DishController.php

--- src/Controller/DishController.php
+++ src/Controller/DishController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Component\Security\Core\Authorization\ExpressionLanguage;
+
 
 class DishController extends AbstractController
 {
